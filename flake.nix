45ac89055c4f7efb3e25f8c47f48332e94ecbf28
li <@li:maisiliym.kriom>
2023-09-11 13:20:40 +0700

    (snapshot successfulEval)

--- flake.nix
+++ flake.nix
@@ -2,14 +2,18 @@
   description = "flakeWorld: Objectifying nix flakes - Nix library for KriomOS";
 
   inputs = {
+    systems = { type = "indirect"; id = "systems"; };
     lib = { type = "indirect"; id = "lib"; };
     nixpkgs = { type = "indirect"; id = "nixpkgs"; };
+
     paisano = { type = "indirect"; id = "paisano"; };
     paisano-tui = { type = "indirect"; id = "paisano-tui"; };
-    dmerge = { type = "indirect"; id = "dmerge"; };
-    yants = { type = "indirect"; id = "yants"; };
+
     blank = { type = "indirect"; id = "blank"; };
+    dmerge = { type = "indirect"; id = "dmerge"; };
     incl = { type = "indirect"; id = "incl"; };
+    nixago = { type = "indirect"; id = "nixago"; };
+    yants = { type = "indirect"; id = "yants"; };
 
     haumea = {
       type = "indirect";
@@ -28,11 +32,23 @@
       id = "std";
       inputs = {
         blank.follows = "blank";
+        devshell.follows = "devshell";
         haumea.follows = "haumea";
+        nixago.follows = "nixago";
         nixpkgs.follows = "nixpkgs";
         yants.follows = "yants";
       };
     };
+
+    devshell = {
+      type = "indirect";
+      id = "devshell";
+      inputs = {
+        systems.follows = "systems";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
   };
 
   outputs = inputs@{ self, std, ... }:
@@ -41,10 +57,11 @@
         inherit inputs;
         cellsFrom = ./cells;
         cellBlocks = with std.blockTypes; [
-          (devshells "devshell")
+          (devshells "shell")
+          (nixago "configs")
         ];
       }
-      { devShells = std.harvest self [ "devshell" "devshell" ]; };
+      { devShells = std.harvest self [ "_local" "shell" ]; };
 }
 
   
